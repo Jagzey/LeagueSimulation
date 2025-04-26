eb0d8ac5f180992ebd6af0072bf1c69e3d595414
Jagzey <bradley72817@gmail.com>
2025-04-26 15:21:14 +0100

    - player class vastly modified from 3000 lines to 500 lines

--- obj/Debug/net8.0-windows/LeagueSimulation.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/LeagueSimulation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeagueSimulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e86c91125c343cd70c4f24f2adec057fdc63d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04de692e1b204453697c5a202d055253702fd0b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeagueSimulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeagueSimulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
